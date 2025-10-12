aa08d5d9563c8c2d392c1a557d071629268ca302
Md. Mohi-Uddin <mohiuddin655.bd@gmail.com>
2025-10-12 13:54:55 +0600

    ## 1.0.8
    * Add AnalyticsEventItem with fields instances

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,5 @@
+## 1.0.8
+
 ## 1.0.7
 
 * Add AnalyticsEventItem with fields instances
--- lib/in_app_analytics.dart
+++ lib/in_app_analytics.dart
@@ -2,3 +2,4 @@ export 'src/analytics.dart';
 export 'src/delegate.dart';
 export 'src/event.dart';
 export 'src/extension.dart';
+export 'src/models.dart';
